fa5c5e9b83382b797e314083f1a1bbbd9e2ca010
HBA114 <hbasriayhaner114@gmail.com>
2023-07-03 23:37:07 +0300

    Implemented BubbleSort

--- SortingAlgorithms/Algorithms/BubbleSort/BubbleSort.cs
+++ SortingAlgorithms/Algorithms/BubbleSort/BubbleSort.cs
@@ -4,11 +4,43 @@ public static class BubbleSort
 {
     public static List<int> Sort(List<int> unsortedList)
     {
-        throw new NotImplementedException();
+        int sortedCount = 0;   
+
+        do
+        {
+            for (int i = 1; i < unsortedList.Count - sortedCount; i++)
+            {
+                if (unsortedList[i - 1] > unsortedList[i])
+                {
+                    int temp = unsortedList[i];
+                    unsortedList[i] = unsortedList[i - 1];
+                    unsortedList[i - 1] = temp;
+                }
+            }
+            sortedCount++;
+        } while (sortedCount < unsortedList.Count);
+        
+        return unsortedList;
     }
-    
+
     public static List<int> SortByDescending(List<int> unsortedList)
     {
-        throw new NotImplementedException();
+        int sortedCount = 0;
+        
+        do
+        {
+            for (int i = 1; i < unsortedList.Count - sortedCount; i++)
+            {
+                if (unsortedList[i - 1] < unsortedList[i])
+                {
+                    int temp = unsortedList[i];
+                    unsortedList[i] = unsortedList[i - 1];
+                    unsortedList[i - 1] = temp;
+                }
+            }
+            sortedCount++;
+        } while (sortedCount < unsortedList.Count);
+        
+        return unsortedList;
     }
 }
--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -1,4 +1,5 @@
-﻿using SortingAlgorithms.Algorithms.SelectionSort;
+﻿using SortingAlgorithms.Algorithms.BubbleSort;
+using SortingAlgorithms.Algorithms.SelectionSort;
 
 List<int> numberList = new() { 1, 4, 2, 9, 0, 3, 7, 6, 5, 8 };
 Console.WriteLine("Original: ");
@@ -10,7 +11,7 @@ Console.WriteLine("");
 
 var result = SelectionSort.Sort(numberList);
 
-Console.WriteLine("Sorted: ");
+Console.WriteLine("Sorted(Selection Sort): ");
 foreach (var item in result)
 {
     Console.Write($"{item} , ");
@@ -19,9 +20,27 @@ Console.WriteLine("");
 
 var resultDescending = SelectionSort.SortByDescending(numberList);
 
-Console.WriteLine($"Sorted Descending: ");
+Console.WriteLine($"Sorted Descending(Selection Sort): ");
 foreach (var item in resultDescending)
 {
     Console.Write($"{item} , ");
 }
 Console.WriteLine($"");
+
+var resultBubble = BubbleSort.Sort(numberList);
+
+Console.WriteLine("Sorted(Bubble Sort): ");
+foreach (var item in resultBubble)
+{
+    Console.Write($"{item} , ");
+}
+Console.WriteLine("");
+
+var resultDescendingBubble = BubbleSort.SortByDescending(numberList);
+
+Console.WriteLine($"Sorted Descending(Bubble Sort): ");
+foreach (var item in resultDescendingBubble)
+{
+    Console.Write($"{item} , ");
+}
+Console.WriteLine("");
